1e770b55aa11994c9dfda4632ad488906fd8caa3
Robert W. Burgholzer <rburghol@vt.edu>
2019-06-25 07:43:24 -0400

    Form alignment (like spray align branch)

--- plugins/om_components/ObjectModelAgmanSprayAppEvent.class.php
+++ plugins/om_components/ObjectModelAgmanSprayAppEvent.class.php
@@ -264,30 +264,24 @@ class ObjectModelAgmanSprayAppEvent extends ObjectModelComponentsDefaultHandler 
       return FALSE;
     }
     parent::buildForm($form, $form_state);
-    $form['name'] = array(
-      '#title' => t('Title'),
-      '#type' => 'textfield',
-      '#default_value' => ($this->dh_adminreg_feature->name <> '') ? $this->dh_adminreg_feature->name : 'Spray Event',
-      '#description' => t('Event Description'),
-      '#required' => TRUE,
-      '#size' => 30,
-      '#weight' => 1,
-    );
-    $date_format = 'Y-m-d H:i';
     // should have code in here to guess based on the phase/or passed in from the URL
     $form['startdate'] = array(
-      '#title' => t('Application Start Date/Time'),
+    // added tags cause theme centers these for some reason - maybe bug in date widget?
+      '#prefix' => "<b>" . t('Application Start Date/Time') . "</b>",
+      '#title' => '',
       '#description' => t('Planned date for this spray.'),
       '#required' => TRUE,
       '#default_value' => empty($this->dh_adminreg_feature->startdate) ? $this->dh_adminreg_feature->startdate : date($date_format,$this->dh_adminreg_feature->startdate),
       '#date_format' => $date_format,
       '#type' => 'date_select',
       '#date_year_range' => '-5:+5',
-      '#weight' => 0,
+      '#weight' => 1,
     );
     // should have code in here to guess based on the phase/or passed in from the URL
     $form['enddate'] = array(
-      '#title' => t('End Date/Time'),
+    // added tags cause theme centers these for some reason - maybe bug in date widget?
+      '#prefix' => "<b>" . t('End/Time') . "</b>",
+      '#title' => '',
       '#description' => t('This will be used to calculate re-entry and post-harvest intervals.'),
       '#required' => FALSE,
       '#default_value' => (empty($this->dh_adminreg_feature->enddate) or ($this->dh_adminreg_feature->enddate < $this->dh_adminreg_feature->startdate)) 
@@ -297,6 +291,15 @@ class ObjectModelAgmanSprayAppEvent extends ObjectModelComponentsDefaultHandler 
       '#type' => 'date_select',
       '#date_year_range' => '-5:+5',
       '#weight' => 2,
+    );
+    $form['name'] = array(
+      '#title' => t('Title'),
+      '#type' => 'textfield',
+      '#default_value' => ($this->dh_adminreg_feature->name <> '') ? $this->dh_adminreg_feature->name : 'Spray Event',
+      '#description' => t('Event Description'),
+      '#required' => TRUE,
+      '#size' => 30,
+      '#weight' => 3,
     );
     //dpm($this->dh_adminreg_feature,"event object");
     $form['show_agchem_event_area'] = array(
@@ -309,9 +312,7 @@ class ObjectModelAgmanSprayAppEvent extends ObjectModelComponentsDefaultHandler 
     );
     //dpm($this->dh_adminreg_feature,"event object");
     $form['agchem_event_area'] = array(
-      '#title' => t('Total Area to Spray'),
       '#type' => 'hidden',
-      '#suffix' => $this->dh_adminreg_feature->dh_properties['agchem_event_area']->varunits,
       '#default_value' => empty($this->dh_adminreg_feature->dh_properties['agchem_event_area']->propvalue) ? 0 : $this->dh_adminreg_feature->dh_properties['agchem_event_area']->propvalue,
       //'#description' => t('Event Description'),
       '#disabled' => FALSE,
