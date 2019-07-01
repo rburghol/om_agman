17bd61fd8a73b6a32c72310e7042ab17667dadad
Robert W. Burgholzer <rburghol@vt.edu>
2019-07-01 06:32:56 -0400

    Removed oselete list of organisms, added debugging calls for advanced.

--- plugins/dh_variables/dHVariablePluginAgmanVitis.class.php
+++ plugins/dh_variables/dHVariablePluginAgmanVitis.class.php
@@ -875,14 +875,6 @@ class dHVariablePluginIPMDisease extends dHVariablePluginIPMIncident {
     $options = dh_varkey_varselect_options(array("vocabulary = 'fungal_pathogens'"));
     asort($options);
     return $options;
-    return array(
-      'org_black_rot' => 'Black Rot',
-      'org_botrytis' => 'Botrytis',
-      'org_black_rot' => 'Downy Mildew',
-      'org_phomopsis' => 'Phomopsis',
-      'org_powdery_mildew' => 'Powdery Mildew',
-      'org_ripe_rot' => 'Ripe Rot',
-    );
   }
   
   public function getDefaults($entity, &$defaults = array()) {
@@ -933,7 +925,7 @@ class dHVariablePluginIPMDisease extends dHVariablePluginIPMIncident {
     $form['Advanced']['#collapsed'] = TRUE;
     $form['Advanced']['#weight'] = 2;
     $adv = $row->Advanced;
-    //dpm($adv,'row');
+    dpm($adv,'row');
     //dpm($adv->propvalue,'propvalue');
     if ($adv->propvalue > 0) {
       // using advanced notation, so show as expanded
