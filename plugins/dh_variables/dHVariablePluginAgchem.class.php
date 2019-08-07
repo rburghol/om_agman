9218d17c2407285e2295a410721f3e232a599d97
Robert W. Burgholzer <rburghol@vt.edu>
2019-08-07 07:06:50 -0400

    debug entity

--- plugins/dh_variables/dHVariablePluginAgchem.class.php
+++ plugins/dh_variables/dHVariablePluginAgchem.class.php
@@ -534,6 +534,7 @@ class dHAgchemApplicationEvent extends dHVariablePluginDefault {
   
   public function update(&$entity) {
     parent::update($entity);
+    dpm($entity,'entity');
     //$feature = $this->getParentEntity($entity);
     //$this->load_event_info($feature);
     //$this->setBlockPHI(&$feature);
