0eea2930eff2131b0f8f54aaf14de01ae7ee7be1
Robert W. Burgholzer <rburghol@vt.edu>
2019-08-13 08:23:50 -0400

    Changed phi rec to tstime is end of spray, tsendtime is end of ph interval -- one more place

--- plugins/dh_variables/dHVariablePluginAgchem.class.php
+++ plugins/dh_variables/dHVariablePluginAgchem.class.php
@@ -651,7 +651,7 @@ class dHAgchemApplicationEvent extends dHVariablePluginDefault {
         // now update to the actual phi date if it is less than the new PHI 
         //dsm("event phi: " . dh_handletimestamp($feature->phi_date) . ", tstime: $phi_rec->tstime ");
         //dsm("as Date event phi: " . $feature->phi_date . ", tstime: " . date('Y-m-d',$phi_rec->tstime));
-        if (dh_handletimestamp($feature->phi_date) > dh_handletimestamp($phi_rec->tstime)) {
+        if (dh_handletimestamp($feature->phi_date) > dh_handletimestamp($phi_rec->tsendtime)) {
           $phi_rec->tstime = dh_handletimestamp($feature->enddate);
           $phi_rec->tsendtime = dh_handletimestamp($feature->phi_date);
           $phi_rec->tsvalue = $feature->adminid;
