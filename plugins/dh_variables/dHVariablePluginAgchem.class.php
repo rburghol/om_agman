09cc4b562e1db579f6b50d7f2cc61f96ba190111
Robert W. Burgholzer <rburghol@vt.edu>
2019-08-20 07:07:40 -0400

    debug

--- plugins/dh_variables/dHVariablePluginAgchem.class.php
+++ plugins/dh_variables/dHVariablePluginAgchem.class.php
@@ -699,7 +699,7 @@ class dHAgchemApplicationEvent extends dHVariablePluginDefault {
         // Retrieve existing PHI timeseries record for this block/year and insure only a single record for each block, per growing year
         $block_phi_ts = $this->getBlockTSPHI($fe, $sstime, $setime);
         dpm($block_phi_event,'biggest phi ');
-        dpm($event_phi_prop, 'this one');
+        dpm($block_phi_ts, 'this one');
         // $block_phi_ts has the actual timeseries record to annotate the previous PHI for this block.
         //   This is what gets updated if the current event has a greater PHI than the previous PHI
         if (!is_object($block_phi_ts)) {
