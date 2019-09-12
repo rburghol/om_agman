6c25a804bd784d4013d8af944d91bc1961f3ccef
Robert W. Burgholzer <rburghol@vt.edu>
2019-09-12 06:54:47 -0400

    Removed labeled from render

--- templates/views-view-table--ipm-planning-grid.tpl.php
+++ templates/views-view-table--ipm-planning-grid.tpl.php
@@ -42,7 +42,7 @@ $efficacy = array(
   2=> 'Good',
   3=> 'Good/Fair',
   4=> 'Fair',
-  '4L'=> 'Labelled',
+//  '4L'=> 'Labelled',
   5=> 'Poor',
   6=> 'None',
 );
@@ -52,7 +52,7 @@ $efficacy_sym = array(
   2=> '+',
   3=> '+',
   4=> '-',
-  '4L'=> 'L',
+ // '4L'=> 'L',
   5=> '--',
   6=> '∅',
 );
@@ -62,7 +62,7 @@ $efficacy_color = array(
   2=> '#33b233',
   3=> '#66cc66',
   4=> '#993599',
-  '4L'=> '#993599',
+ // '4L'=> '#993599',
   5=> '#ccffcc',
   6=> '#ffffff',
 );
