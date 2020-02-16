16bd684139a7d83f1ab899fb61e89f541ee3811d
Robert W. Burgholzer <rburghol@vt.edu>
2020-02-16 09:18:48 -0500

    Add buttons, use same blocks if same vineyard

--- src/clone_event.php
+++ src/clone_event.php
@@ -2,6 +2,6 @@
   module_load_include('module', 'dh');
   module_load_include('inc', 'dh', 'plugins/dh.display');
   module_load_include('module', 'om_agman');
-  module_load_include('inc', 'om', 'src/om_agman_clone_event');
+  module_load_include('inc', 'om_agman', 'src/om_agman_clone_event');
     
 ?>
