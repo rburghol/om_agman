14ec653cd46210f76f850461a5f2b2c70e4e6175
Robert W. Burgholzer <rburghol@vt.edu>
2020-05-26 08:11:45 -0400

    Moved advanced section to below tsvalue

--- plugins/dh_variables/dHVariablePluginAgmanVitis.class.php
+++ plugins/dh_variables/dHVariablePluginAgmanVitis.class.php
@@ -586,10 +586,10 @@ class dHVariablePluginIPMIncidentExtent extends dHVariablePluginPercentSelector 
     // get all FRAC Codes associated with this entity
     // Note: Views result sets MUST have tid column included, even if hidden, in order to show a rendered ts entity.
     $codes = $this->incidentCodes();
-    $incident_detail = !empty($entity->tscode) and isset($codes[$entity->tscode]) ? $codes[$entity->tscode] : $varname;
     $feature = $this->getParentEntity($entity);
     $varinfo = $entity->varid ? dh_vardef_info($entity->varid) : FALSE;
     $varname = $varinfo->varname;
+    $incident_detail = !empty($entity->tscode) and isset($codes[$entity->tscode]) ? $codes[$entity->tscode] : $varname;
     if ($varinfo === FALSE) {
       return;
     }
