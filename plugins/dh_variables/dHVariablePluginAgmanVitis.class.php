1a45c08f62e9b3141092a8746430aa9cd8b4e616
Robert W. Burgholzer <rburghol@vt.edu>
2020-07-03 10:25:50 -0400

    save the link

--- plugins/dh_variables/dHVariablePluginAgmanVitis.class.php
+++ plugins/dh_variables/dHVariablePluginAgmanVitis.class.php
@@ -1383,7 +1383,7 @@ class dHAgmanSVSampleEvent extends dHVariablePluginAgmanAction {
       // @todo: once this goes into the dHOMLinkage plugin we can delete call to save this property 
       if ($prop->linked_ts->is_new) {
         $prop->linked_ts->tsvalue = $tid;
-        $prop->save();
+        $prop->linked_ts->save();
       }
       // iterate through replicant_proplist and copy from parent to replicant 
       /*
