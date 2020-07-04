052de4a70d1943fd35e09099d87b5046da185ad6
Robert W. Burgholzer <rburghol@vt.edu>
2020-07-03 21:32:16 -0400

    use multiplicity tstime for entity creation

--- plugins/dh_variables/dHVariablePluginAgmanVitis.class.php
+++ plugins/dh_variables/dHVariablePluginAgmanVitis.class.php
@@ -1382,6 +1382,7 @@ class dHAgmanSVSampleEvent extends dHVariablePluginAgmanAction {
         );
         $ts = entity_create('dh_timeseries', $ts_info); // says get all matching tstime
         dpm($ts, 'ts');
+        $ts->save();
       }
       // @todo: once this goes into the dHOMLinkage plugin we can delete call to save this property 
       if ($prop->linked_ts->is_new or ($prop->linked_td->propvalue == NULL)) {
