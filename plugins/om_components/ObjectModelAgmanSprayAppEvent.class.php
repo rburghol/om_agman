ce142746d34e746d8f761747c18f441d3a91f022
Robert W. Burgholzer <rburghol@vt.edu>
2021-05-03 15:42:07 -0400

    debug

--- plugins/om_components/ObjectModelAgmanSprayAppEvent.class.php
+++ plugins/om_components/ObjectModelAgmanSprayAppEvent.class.php
@@ -415,6 +415,7 @@ class ObjectModelAgmanSprayAppEvent extends ObjectModelComponentsDefaultHandler 
     $form['chem_rates']['#prefix'] .= t('Notice: This application is designed to be an aid to help your pesticide use planning. However, it is your responsibility to keep, read, and follow the labels and SDS.');
     $form['chem_rates']['#prefix'] .= '</span>';
     $form['chem_rates']['#suffix'] = '</div">';
+    /*
     $desc = $this->dh_adminreg_feature->description['und'][0]['value'];
     $form['description'] = array(
       '#type' => 'textarea',
@@ -424,6 +425,7 @@ class ObjectModelAgmanSprayAppEvent extends ObjectModelComponentsDefaultHandler 
       '#rows' => 5,
       '#weight' => 7
     );  
+    */
     $form['data']['#tree'] = TRUE;
     $form['actions'] = array('#type' => 'actions');
     $form['actions']['submit'] = array(
