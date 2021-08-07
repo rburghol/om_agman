283ec60a1392401cf387e8fdf2cb18cfb56b321c
Robert W. Burgholzer <rburghol@vt.edu>
2021-08-07 09:46:03 -0400

    set full percent rage (or better anyhow)

--- plugins/dh_variables/dHVariablePluginAgmanVitis.class.php
+++ plugins/dh_variables/dHVariablePluginAgmanVitis.class.php
@@ -591,8 +591,8 @@ class dHVariablePluginIPMIncidentExtent extends dHVariablePluginPercentSelector 
     }
     $pcts[] = '>95';
     $pcts = $this->pct_list($pcts);
-    */
     $form['tsvalue']['#options'] = $pcts;
+    */
     $form['tsvalue']['#description'] = t('% of Plants Affected.  To use incident/extent notation click below to expand the section labeled Advanced');
     $form['tsvalue']['#weight'] = 3;
     
