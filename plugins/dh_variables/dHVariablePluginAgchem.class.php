78c3c417f6691b9b4bb6e893235508faed5738f4
Robert W. Burgholzer <rburghol@vt.edu>
2021-10-22 07:41:29 -0400

    test adding new render

--- plugins/dh_variables/dHVariablePluginAgchem.class.php
+++ plugins/dh_variables/dHVariablePluginAgchem.class.php
@@ -1061,7 +1061,8 @@ class dHAgchemApplicationEvent extends dHVariablePluginDefault {
       // note, this is not detailed in the modes available in dh.module, so this will not be available in Views
         $content = array('body'=>array());
         #$content['body']['#type']= 'item'; 
-        $content['body']['#markup'] .= "<b>Spray:</b> $feature->chem_list " . $edit_l_tiny;
+        $chem_list = implode(", ", $feature->chem_names);
+        $content['body']['#markup'] .= "<b>Spray:</b> $chem_list " . $edit_l_tiny;
       break;
       
       case 'full':
