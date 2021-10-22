717162125a0d42fd5a7fe912ef951c3384493ffe
Robert W. Burgholzer <rburghol@vt.edu>
2021-10-21 20:51:59 -0400

    basic report sql developed, test page added

--- src/sql/block_events.sql
+++ src/sql/block_events.sql
@@ -6,7 +6,7 @@
 --     timeseries record that the adminreg record is linked to, since that has the tidy summary.
 --     but really, the tidy summary should be accesible from the linked ts rec 
 (
-	select ts.tid, ts.tstime, ts.tsvalue, ts.tscode, ts.entity_type, ts.varid,
+	select ts.tid, ts.tstime, to_timestamp(ts.tstime), ts.tsvalue, ts.tscode, ts.entity_type, ts.varid,
 	  feat.hydroid, feat.name, feat.bundle 
 	from dh_timeseries as ts 
 	left outer join 
@@ -16,12 +16,13 @@
 	  and ts.entity_type = 'dh_feature'
 	)
 	where 
-	ts.entity_type = 'dh_feature'
-	and featureid = 147
+	ts.entity_type = 'dh_feature' 
+	and featureid = 147 
+	and varid not in (select hydroid from dh_variabledefinition where varkey in ('event_dha_default', 'event_dh_link_submittal_feature') )
 	and tstime >= extract(epoch from '2021-01-01'::timestamp)
 	and tstime <= extract(epoch from '2021-12-31'::timestamp)
 ) UNION (
-  select ts.tid, ts.tstime, ts.tsvalue, ts.tscode, ts.entity_type, ts.varid,
+  select ts.tid, ts.tstime, to_timestamp(ts.tstime), ts.tsvalue, ts.tscode, ts.entity_type, ts.varid,
 	  arfeat.adminid, arfeat.name, arfeat.bundle 
 	from dh_adminreg_feature as arfeat
 	left outer join field_data_dh_link_feature_submittal as link
@@ -38,8 +39,10 @@
 	  and ts.entity_type = 'dh_adminreg_feature'
 	)
 	where link.dh_link_feature_submittal_target_id = 147 
+	-- filter out varid 191 which is created by default for every adminreg record to say "adminreg record created" 
 	and ts.varid <> 191 
 	and arfeat.startdate >= extract(epoch from '2021-01-01'::timestamp)
 	and arfeat.startdate <= extract(epoch from '2021-12-31'::timestamp)
-)
+) 
+order by tstime 
 ;
