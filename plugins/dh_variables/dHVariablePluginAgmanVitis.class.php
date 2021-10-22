c4978b27c953e16c011f6f3baf7dfef4b3b97682
Robert W. Burgholzer <rburghol@vt.edu>
2021-10-22 10:05:41 -0400

    tss added to tiny chem

--- plugins/dh_variables/dHVariablePluginAgmanVitis.class.php
+++ plugins/dh_variables/dHVariablePluginAgmanVitis.class.php
@@ -1224,7 +1224,7 @@ class dHVariablePluginFruitChemSample extends dHVariablePluginAgmanAction {
         $content = array();
         $content['body'] = array(
           '#type' => 'item',
-          '#markup' => "$varname @ " . $entity->tss->propvalue . " brix in " . $feature->name,
+          //'#markup' => "$varname @ " . $entity->tss->propvalue . " brix in " . $feature->name,
         );
       break;
       case 'tiny':
