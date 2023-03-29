ea7e67181ab6fbaf92fa6e567c9d51944bed8f90
Robert W. Burgholzer <rburghol@vt.edu>
2023-03-28 22:41:59 -0400

    debug off

--- js/om_agman.js
+++ js/om_agman.js
@@ -1,7 +1,7 @@
 // Work in progress - goal is to do all auto-calcs
 
 function om_agman_rate_total(span_field_index) {
-	console.log(event.target.id)
+	//console.log(event.target.id)
 	rfid = event.target.id // in form of edit-chem-rates-N-rate-propvalue
     rf_pieces = rfid.split('-')
 	rfix = rf_pieces[3]
@@ -10,7 +10,7 @@ function om_agman_rate_total(span_field_index) {
 	batch_id = 'batch-vol-' + rfix
 	total_id = 'total-vol-' + rfix
 	units_id = 'amount-units-' + rfix
-	console.log(unitconv_id)
+	//console.log(unitconv_id)
 	area_id = "edit-event-settings-4-propvalue"
 	area_acres = document.getElementById(area_id).value
 	unit_conv = document.getElementById(unitconv_id).value
@@ -18,7 +18,7 @@ function om_agman_rate_total(span_field_index) {
 	total_vol = document.getElementById(total_id).value
 	units = document.getElementById(units_id).value
 	total_amount = rate * unit_conv
-	console.log(total_amount)
+	//console.log(total_amount)
 	// uses rounding muti digit advice in javascript from https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary
 	if (total_amount > 10) {
 		total_amount = Math.round( ( total_amount + Number.EPSILON ) * 10 ) / 10;
@@ -28,10 +28,10 @@ function om_agman_rate_total(span_field_index) {
 	//total_amount = (total_amount > 10) ? Math.round(total_amount,1) : Math.round(total_amount,2);
 	batch_amount = total_amount * batch_vol / total_vol;
     batch_amount = (batch_amount > 10) ? Math.round(batch_amount,1) : Math.round(batch_amount,2);
-	console.log(rate)
-	console.log(unit_conv)
-	console.log(total_amount)
-	console.log(batch_amount)
+	//console.log(rate)
+	//console.log(unit_conv)
+	//console.log(total_amount)
+	//console.log(batch_amount)
 	amt_str = total_amount + ' ' + units
 	if (batch_vol < total_vol) {
 		amt_str = amt_str + " (" + batch_amount + ' ' + units + ' per tank'
