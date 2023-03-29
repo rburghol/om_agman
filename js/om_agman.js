5c343368c6315607e548c767047bffcb712152fc
Robert W. Burgholzer <rburghol@vt.edu>
2023-03-28 21:03:15 -0400

    try conv again

--- js/om_agman.js
+++ js/om_agman.js
@@ -7,12 +7,12 @@ function om_agman_rate_total(span_field_index) {
 	rfix = rf_pieces[3]
 	rate = document.getElementById(rfid).value
 	unitconv_id = 'chem_rates[' + rfix + '][unitconv]'
+	console.log(unitconv_id)
 	area_id = "edit-event-settings-4-propvalue"
 	area_acres = document.getElementById(area_id).value
 	unit_conv = document.getElementById(unitconv_id).value
 	total_amount = rate
 	console.log(total_amount)
-	console.log(unitconv_id)
 	console.log(unit_conv)
 	//document.getElementById(span_field_index).innerHTML = total_amount;
 }
