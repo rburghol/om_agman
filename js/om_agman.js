72e677799c127cb5fe49fb714f95c4a7a08e98db
Robert W. Burgholzer <rburghol@vt.edu>
2023-03-28 20:06:49 -0400

    quotes

--- js/om_agman.js
+++ js/om_agman.js
@@ -1,6 +1,6 @@
 // Work in progress - goal is to do all auto-calcs
 
 function om_agman_rate_total(rate_field_index, span_field_index) {
-	console.log(rate_field_index)
+	console.log(event.target.id)
 	//document.getElementById(span_field_index).innerHTML = document.getElementById(rate_field_index).value;
 }
