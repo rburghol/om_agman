76e72113469a9c8243b799628cdefc99587c33e3
Robert W. Burgholzer <rburghol@vt.edu>
2023-03-28 21:44:37 -0400

    correct name

--- js/om_agman.js
+++ js/om_agman.js
@@ -17,7 +17,7 @@ function om_agman_rate_total(span_field_index) {
 	total_vol = document.getElementById(total_id).value
 	total_amount = rate * unit_conv
 	batch_amount = total_amount * batch_vol / total_vol;
-    batch_amount = (batch_amount > 10) ? round(batch_amount,1) : round(batch_amount,2);
+    batch_amount = (batch_amount > 10) ? Math.round(batch_amount,1) : Math.round(batch_amount,2);
 	console.log(rate)
 	console.log(unit_conv)
 	console.log(total_amount)
