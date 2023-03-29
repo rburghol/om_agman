c3773d73b8bd8ac4032fbd9e68c20c790f38f53d
Robert W. Burgholzer <rburghol@vt.edu>
2023-03-28 20:00:12 -0400

    debug

--- js/om_agman.js
+++ js/om_agman.js
@@ -2,5 +2,5 @@
 
 function om_agman_rate_total(rate_field_index, span_field_index) {
 	alert(rate_field_index)
-	document.getElementById(span_field_index).innerHTML = document.getElementById(rate_field_index).value;
+	//document.getElementById(span_field_index).innerHTML = document.getElementById(rate_field_index).value;
 }
